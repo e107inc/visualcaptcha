ca35b9642c14b0923ffc99824c3a371e003b40ff
Lóna Lore <lonalore@freemail.hu>
2016-12-04 23:08:12 +0100

    Use function to get captcha for form.

--- visualcaptcha.php
+++ visualcaptcha.php
@@ -13,41 +13,7 @@ if(session_id() == '')
 require_once("../../class2.php");
 
 $form = e107::getForm();
-$tp = e107::getParser();
 $msg = e107::getMessage();
-
-
-$captchaSettings = array();
-
-$captchaSettings[] = array(
-	'namespace' => 'captcha_test_form',
-	'formID'    => 'captcha-test-form',
-	'imgPath'   => e_PLUGIN . 'visualcaptcha/img/',
-	'url'       => e_PLUGIN . 'visualcaptcha/app.php',
-	'imgCount'  => 4,
-	'language'  => array(
-		'accessibilityAlt'         => 'Sound icon',
-		'accessibilityTitle'       => 'Accessibility option: listen to a question and answer it!',
-		'accessibilityDescription' => 'Type below the <strong>answer</strong> to what you hear. Numbers or words:',
-		'explanation'              => 'Click or touch the <strong>ANSWER</strong>',
-		'refreshAlt'               => 'Refresh/reload icon',
-		'refreshTitle'             => 'Refresh/reload: get new images and accessibility option!',
-	)
-);
-
-e107::js('settings', array('visualcaptcha' => $captchaSettings));
-e107::js('visualcaptcha', 'js/visualcaptcha.bootstrap.js');
-
-if(e_DEBUG)
-{
-	e107::css('visualcaptcha', 'css/visualcaptcha.src.css');
-	e107::js('visualcaptcha', 'js/visualcaptcha.jquery.src.js');
-}
-else
-{
-	e107::css('visualcaptcha', 'css/visualcaptcha.css');
-	e107::js('visualcaptcha', 'js/visualcaptcha.jquery.js');
-}
 
 
 require_once(HEADERF);
@@ -68,13 +34,59 @@ if(!empty($_POST['namespace']))
 	echo $msg->render();
 }
 
-echo $form->open('captcha_test_form', 'POST', e_SELF);
-echo '<div class="e-visual-captcha"></div>';
+$form_name = 'captcha_test_form';
+
+echo $form->open($form_name, 'POST', e_SELF);
+echo toCaptcha($form_name);
 echo $form->submit('submit', 'Submit');
 echo $form->close();
 
 require_once(FOOTERF);
 exit;
+
+
+/**
+ * @param $form_name
+ * @return
+ */
+function toCaptcha($form_name)
+{
+	$form = e107::getForm();
+
+	$captchaSettings = array();
+
+	$captchaSettings[] = array(
+		'namespace' => $form_name,
+		'formID'    => $form->name2id($form_name),
+		'imgPath'   => e_PLUGIN . 'visualcaptcha/img/',
+		'url'       => e_PLUGIN . 'visualcaptcha/app.php',
+		'imgCount'  => 4,
+		'language'  => array(
+			'accessibilityAlt'         => 'Sound icon',
+			'accessibilityTitle'       => 'Accessibility option: listen to a question and answer it!',
+			'accessibilityDescription' => 'Type below the <strong>answer</strong> to what you hear. Numbers or words:',
+			'explanation'              => 'Click or touch the <strong>ANSWER</strong>',
+			'refreshAlt'               => 'Refresh/reload icon',
+			'refreshTitle'             => 'Refresh/reload: get new images and accessibility option!',
+		)
+	);
+
+	e107::js('settings', array('visualcaptcha' => $captchaSettings));
+	e107::js('visualcaptcha', 'js/visualcaptcha.bootstrap.js');
+
+	if(e_DEBUG)
+	{
+		e107::css('visualcaptcha', 'css/visualcaptcha.src.css');
+		e107::js('visualcaptcha', 'js/visualcaptcha.jquery.src.js');
+	}
+	else
+	{
+		e107::css('visualcaptcha', 'css/visualcaptcha.css');
+		e107::js('visualcaptcha', 'js/visualcaptcha.jquery.js');
+	}
+
+	return '<div class="e-visual-captcha"></div>';
+}
 
 /**
  * @return bool
